3705e970566a9591f1e190f7eaa553b3cdb51266
kinghsm <kinghsm32@hotmail.com>
2020-04-06 01:09:14 +0200

    Mise à jour incrément
    Corrections divers

--- Increment/LoveMirroringTestMatch/LoveMirroringTestMatch/Pages/Matching.cshtml.cs
+++ Increment/LoveMirroringTestMatch/LoveMirroringTestMatch/Pages/Matching.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 //using LoveMirroringTestMatch.Controllers;
@@ -36,12 +37,15 @@ namespace LoveMirroringTestMatch
 
 
         public class InputModel
-        { 
+        {
+            [Required]
             public string Sexe { get; set; }
+            [Required]
             public int Age { get; set; }
+            [Required]
             public string Profil { get; set; }
         }
-        public async Task OnGetAsync(string returnUrl = "~/Matching")
+        public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
             AspNetUsers user = null;
@@ -60,16 +64,17 @@ namespace LoveMirroringTestMatch
         //    Choices = await _matchingController.GetUsersChoice(Input.Sexe, Input.Age, Input.Profil);
         //}
 
-        public async Task OnPostSearchAsync(string sexe, int agemax, string profil)
+        public async Task OnPostSearchAsync(string returnUrl = null)
         {
+            returnUrl = returnUrl ?? Url.Content("~/");
             var usersChoices = from u in await _context.AspNetUsers.ToListAsync()
-                               where u.Birthday.Year - DateTime.Now.Year <= agemax
+                               where u.Birthday.Year - DateTime.Now.Year <= Input.Age
                                join s in await _context.Sexes.ToListAsync() on u.SexeId equals s.SexeId
-                               where s.SexeName.Equals(sexe)
+                               where s.SexeName.Equals(Input.Sexe)
                                join us in await _context.UsersProfils.ToListAsync() on u.Id equals us.Id
                                join p in await _context.Profils.ToListAsync() on us.ProfilId equals p.ProfilId
-                               where p.ProfilName.Equals(profil)
-                               select new UserChoiceViewModel { UserName = u.UserName, Age = agemax, Sexe = s.SexeName, Profil = p.ProfilName };
+                               where p.ProfilName.Equals(Input.Profil)
+                               select new UserChoiceViewModel { UserName = u.UserName, Age = u.Birthday.Year - DateTime.Now.Year, Sexe = s.SexeName, Profil = p.ProfilName };
 
             Choices = new UsersChoicesViewModel { UsersChoices = usersChoices };
         }
