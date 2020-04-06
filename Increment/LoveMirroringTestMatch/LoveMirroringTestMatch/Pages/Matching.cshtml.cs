b998ba3226eb60c95e38bab9953aa7afe14633f0
kinghsm <kinghsm32@hotmail.com>
2020-04-06 10:23:35 +0200

    Mise à jour incrément
    Recherche rendue fonctionnel

--- Increment/LoveMirroringTestMatch/LoveMirroringTestMatch/Pages/Matching.cshtml.cs
+++ Increment/LoveMirroringTestMatch/LoveMirroringTestMatch/Pages/Matching.cshtml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-//using LoveMirroringTestMatch.Controllers;
 using LoveMirroringTestMatch.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
@@ -15,14 +14,11 @@ namespace LoveMirroringTestMatch
 {
     public class MatchingModel : PageModel
     {
-        //private readonly MatchingController _matchingController;
         private readonly LoveMirroringContext _context;
         private readonly UserManager<AspNetUsers> _userManager;
 
-        //public MatchingModel(MatchingController matching, LoveMirroringContext context, UserManager<AspNetUsers> userManager)
         public MatchingModel(LoveMirroringContext context, UserManager<AspNetUsers> userManager)
         {
-            //_matchingController = matching;
             _context = context;
             _userManager = userManager;
         }
@@ -58,23 +54,27 @@ namespace LoveMirroringTestMatch
             
         }
 
-        //public async Task OnPostAsync(string returnUrl = null)
-        //{
-        //    ReturnUrl = returnUrl;
-        //    Choices = await _matchingController.GetUsersChoice(Input.Sexe, Input.Age, Input.Profil);
-        //}
-
         public async Task OnPostSearchAsync(string returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
+
+            AspNetUsers user = null;
+            user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                Sexes = _context.Sexes.ToList();
+                Profils = _context.Profils.ToList();
+            }
+
             var usersChoices = from u in await _context.AspNetUsers.ToListAsync()
-                               where u.Birthday.Year - DateTime.Now.Year <= Input.Age
+                               where DateTime.Now.Year - u.Birthday.Year <= Input.Age
+                               && u.Id != user.Id
                                join s in await _context.Sexes.ToListAsync() on u.SexeId equals s.SexeId
                                where s.SexeName.Equals(Input.Sexe)
                                join us in await _context.UsersProfils.ToListAsync() on u.Id equals us.Id
                                join p in await _context.Profils.ToListAsync() on us.ProfilId equals p.ProfilId
                                where p.ProfilName.Equals(Input.Profil)
-                               select new UserChoiceViewModel { UserName = u.UserName, Age = u.Birthday.Year - DateTime.Now.Year, Sexe = s.SexeName, Profil = p.ProfilName };
+                               select new UserChoiceViewModel { UserName = u.UserName, Age = DateTime.Now.Year - u.Birthday.Year, Sexe = s.SexeName, Profil = p.ProfilName };
 
             Choices = new UsersChoicesViewModel { UsersChoices = usersChoices };
         }
