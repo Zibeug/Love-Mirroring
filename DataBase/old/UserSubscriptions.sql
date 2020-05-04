d09e928408f62ed5653e4bf8ba39799dc03173c4
Tim <tim.allemann@outlook.com>
2020-05-04 15:39:29 +0200

    Ajout de la table Paiement d'abonnement (UserSubscription)

--- DataBase/old/UserSubscriptions.sql
+++ DataBase/old/UserSubscriptions.sql
@@ -5,16 +5,19 @@ USE [LoveMirroring]
 GO
 create table UserSubscriptions
   (
-     SubscriptionID int IDENTITY(1,1) NOT NULL,
+     UserSubscriptionsID int IDENTITY(1,1) NOT NULL,
      UserID [nvarchar](450)  null  ,
-     SubscriptionDATE datetime  not null  ,
-	 SubscriptionAmount decimal  not null  ,
-     constraint PK_SubscriptionID primary key (SubscriptionID)
+     UserSubscriptionsDATE datetime  not null  ,
+	 UserSubscriptionsAmount decimal  not null  ,
+	 SubscriptionsID [smallint] not null,
+     constraint PK_UserSubscriptionsIDID primary key (UserSubscriptionsID)
   ) 
 go
 
 alter table UserSubscriptions 
      add constraint FK_UserSubscriptions_ASPNETUSERS foreign key (UserID) 
                references ASPNETUSERS (ID)
-			   ON DELETE CASCADE
+			   ON DELETE CASCADE,
+	 add constraint FK_Subscriptions_ASPNETUSERS foreign key (SubscriptionsID) 
+               references Subscriptions ([SubscriptionId])
 go
